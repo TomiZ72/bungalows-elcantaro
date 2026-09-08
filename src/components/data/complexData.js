feca597af6ecbd6874489e51b9a73b2e90267ebf
tomaszalazar <tomy.nahuel.zalazar@gmail.com>
2026-09-08 18:56:17 -0300

    add: data folder and clean project

---
+++ src/components/data/complexData.js
@@ -0,0 +1,21 @@
+const complex = {
+    description:
+        'Un espacio para descansar y compartir buenos momentos durante tu estadía en Villa Elisa.',
+
+    location: {
+        address: 'Avenida Mitre 2075',
+        city: 'Villa Elisa',
+        province: 'Entre Ríos',
+        country: 'Argentina',
+    },
+
+    amenities: [
+        'Piscina cubierta',
+        'Patio',
+        'Estacionamiento',
+        'Parrilla',
+        'Juegos para niños',
+        'Wi-Fi',
+    ],
+};
+export { complex }
